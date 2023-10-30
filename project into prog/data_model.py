ab1086a804f9007f31badcc10ee44319a54bbccb
qiaomin <uccaanf@ucl.ac.uk>
2023-10-30 23:26:06 +0000

    test

--- project into prog/data_model.py
+++ project into prog/data_model.py
@@ -41,4 +41,4 @@ class EmergencyProfile:
         self.number_of_relatives = number_of_relatives
         self.timestamp = timestamp
         #test joe 22231x123
-        #adsadasdas
+        #adsadasdascccc
