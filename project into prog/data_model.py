ba1752f0b72f5d771b7d520e937a7cc104dadced
qiaomin <uccaanf@ucl.ac.uk>
2023-10-30 23:56:48 +0000

    testwww

--- project into prog/data_model.py
+++ project into prog/data_model.py
@@ -40,4 +40,4 @@ class EmergencyProfile:
         self.lead_family_member = lead_family_member
         self.number_of_relatives = number_of_relatives
         self.timestamp = timestamp
-        #test joe   sssss123423477788
+        #test joe   sssss12342347778899
