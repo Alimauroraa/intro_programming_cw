169d0200f5c127e2342c88b241f909b48451ad1a
Aur01 <elfidaalima@gmail.com>
2023-11-15 12:08:38 +0000

    Add logging file, create_plan file, close_plan file

--- project into prog/create_plan.py
+++ project into prog/create_plan.py
@@ -1,13 +1,9 @@
 import pandas as pd
-from datetime import date as d
+from datetime import date as d, datetime as dt
 import logging
 
 logging.basicConfig(level=logging.INFO,filename='create_plan_logging.log',
                     format="%(asctime)s - %(levelname)s - %(message)s", datefmt='%d-%b-%y %H:%M:%S')
-
-
-#importing the csv file for plan
-plan_df=pd.read_csv("plan.csv")
 
 #class for the plan
 class HumanitarianPlan:
@@ -67,31 +63,99 @@ class HumanitarianPlan:
                                                     'active', 'NumberOfCamps', 'campID'])
         added_df.to_csv("plan.csv", mode='a',header=False, index=False)
 
-    def close_plan(self):
-        now = d.now()
-        current_date = now.strftime("$y-%m-%d")
-        print(current_date)
+    # def close_plan(self):
+    #     now = d.now()
+    #     current_date = now.strftime("$y-%m-%d")
+    #     print(current_date)
 
 
 
+#importing the csv file for plan
+plan_df=pd.read_csv("plan.csv")
+admin_df=pd.read_csv("admin_credentials.csv")
+print(plan_df.to_string()) #display the whole table for plan_df by putting to_string()
+print(admin_df.to_string())
+print(admin_df['user_id'].tolist())
 
-#------------------INPUT------------------#
-input_admin_id = input("Enter admin id:")
-input_plan_name = input("Enter plan name:")
-logging.info(f"Plan name: {input_plan_name}")
-input_description = input("Enter description:")
-input_geographical_area = input("Enter geographical area:")
-logging.info(f"Plan location: {input_geographical_area}")
-input_start_date = input("Enter start date (YYYY-MM-DD):")
-input_closing_date = input("Enter closing date (YYYY-MM-DD):")
-try:
-    start_date = d.fromisoformat(input_start_date)
-    closing_date = d.fromisoformat(input_closing_date)
-except ValueError as e:
-    print ("Not in correct format")
-input_active = 1
-input_number_camps=input("Enter number of camps:")
+#-----------------------------------------------INPUT------------------------------------------------------------#
 
+while True:     #condition is true by default and we need to
+    input_admin_id = input("Enter admin id:")
+    if input_admin_id.isdigit() and input_admin_id in map(str,admin_df['user_id'].tolist()):    #this will check whether the value exists in rows within user_id column
+        #map(str,..) converts the row into string so it can be compared with the input
+        logging.info(f"Admin id: {input_admin_id}")
+        break
+    elif not input_admin_id.isdigit():
+        print("Invalid input. Please enter a numeric value.")
+    else:
+        print("Admin id not found. Please try again.")
+
+while True:
+    input_plan_name = input("Enter plan name:")
+    if all(char.isalpha() or char.isspace() for char in input_plan_name) and 1< len(input_plan_name) < 200:
+        logging.info(f"Plan name: {input_plan_name}")
+        break
+    elif not all(char.isalpha() or char.isspace() for char in input_plan_name):
+        print("Please enter only alphabetical character.")
+
+    elif len(input_plan_name) > 200:
+        print("200 characters only.")
+    elif len(input_plan_name) == 1:
+        print("Invalid input. Please try again")
+
+while True:
+    input_description = input("Enter description:")
+    if 1<len(input_description)<500:
+        break
+    elif len(input_description)>500:
+        print("500 characters only")
+    if len(input_description)==1:
+        print("Invalid input. Please try again")
+
+while True:
+    input_geographical_area = input("Enter geographical area:")
+    if all(char.isalpha() or char.isspace() for char in input_geographical_area) and 1<len(input_geographical_area)<200:
+        logging.info(f"Plan location: {input_geographical_area}")
+        break
+    elif not all(char.isalpha() or char.isspace() for char in input_geographical_area):
+        print("Please enter only alphabetical character.")
+    elif len(input_geographical_area)>200:
+        print("200 characters only")
+
+while True:
+    input_start_date = input("Enter start date (YYYY-MM-DD):")
+    try:
+        start_date = d.fromisoformat(input_start_date)
+        if start_date >= dt.now().date():
+            break
+        else:
+            print("Invalid start date. Please enter again.")
+    except ValueError as e:
+        print("Invalid date format. Please enter in format YYYY-MM-DD")
+
+while True:
+    input_closing_date = input("Enter closing date (YYYY-MM-DD):")
+    try:
+        closing_date = d.fromisoformat(input_closing_date)
+        if closing_date > dt.now().date() and closing_date > start_date:
+            break
+        else:
+            print("Invalid closing date. Please enter again.")
+
+    except ValueError as e:
+        print("Invalid date format. Please enter in format YYYY-MM-DD")
+
+input_active = 1    #by default the plan is active upon creation
+
+while True:
+    input_number_camps=input("Enter number of camps:")
+    if input_number_camps.isdigit():
+        break
+    else:
+        print("Invalid input. Please enter a numeric value.")
+
+
+#calling class and method
 add_plan=HumanitarianPlan(input_admin_id,input_plan_name,input_description,input_geographical_area,
                           input_active,start_date,closing_date,input_number_camps)
 add_plan.create_plan()
