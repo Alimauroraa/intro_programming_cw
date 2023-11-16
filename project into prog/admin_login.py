c2dae9b59c9193f3e7fb53cd03e8bc544cfcf0ce
stellachampeaux <zchasc6@ucl.ac.uk>
2023-11-16 12:23:54 +0000

    updated admin login

--- project into prog/admin_login.py
+++ project into prog/admin_login.py
@@ -1,7 +1,6 @@
-import pandas as pd
-from data_model import Admin
+
 # Read the CSV file into a DataFrame
-admin_df = pd.read_csv('admin_credentials.csv')
+
 # class InvalidLogin(Exception):
 #     """This is an exception handling class that rejects incorrect usernames. Made by stella 06/11/23"""
 #     pass
@@ -14,20 +13,27 @@ admin_df = pd.read_csv('admin_credentials.csv')
 # except:
 #     print("Exception occurred: the username or password you have entered is wrong.")
 
-def AdminLogIn(username,password):
-    user = admin_df[(admin_df['username'] == username) & (admin_df['password'] == password)]
-    if not user.empty:
-        print("Access granted",username,"!")
-    else:
-        print("The username or password you have entered is wrong.")
 
-AdminLogIn("admin",111)
 
-def DisplayVolunteers(csv):
-    """This is a function which takes a csv and displays it - for the purpose of displaying the Volunteer credentials csv file """
-    for index, row in csv.iterrows():
-        print(row)
+import pandas as pd
 
-DisplayVolunteers(admin_df)
+admin_df = pd.read_csv('admin_credentials.csv')
 
+class Admin:
+    def __init__(self, username, password, email):
+        self.username = username
+        self.password = password
+        self.email = email
+        self.created_plans = []  # List of HumanitarianPlans created by this admin
+
+    def AdminLogIn(self):
+        while True:
+            user = admin_df[(admin_df['username'] == self.username) & (admin_df['user_password'] == self.password)]
+            if not user.empty:
+                print("Access granted", self.username, "!")
+                break
+            else:
+                print("The username or password you have entered is wrong.")
+                self.username = input("Please re-enter your username: ")
+                self.password = int(input("Please re-enter your password: "))
 
