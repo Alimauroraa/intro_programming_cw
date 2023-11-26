209425a271bf8f130ce25a6b2e70ba9a1f3519a3
Maryam Badghisi <zcbtadg@ucl.ac.uk>
2023-11-26 18:40:46 +0000

    delete account

--- project into prog/EditVolunteerAccount.py
+++ project into prog/EditVolunteerAccount.py
@@ -16,14 +16,18 @@ def load_volunteers_from_csv(volunteers_file):
 
 # Function to save updated volunteer data to the CSV file
 def save_volunteers_to_csv(volunteers_file, volunteers):
-    with open(volunteers_file, mode='w', newline='') as file:
-        fieldnames = ['user_id', 'username', 'user_password', 'first_name', 'last_name', 'dob', 'user_email',
-              'contact_number', 'address1', 'address2', 'city', 'acc_type', 'availability', 'gender', 'active',
-              'camp_id', 'emergency_profiles']
-        writer = csv.DictWriter(file, fieldnames=fieldnames)
-        writer.writeheader()
+    with open(volunteers_file, mode='w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(['user_id', 'username', 'user_password', 'first_name', 'last_name', 'dob', 'user_email', 'contact_number', 'address1', 'address2', 'city', 'acc_type', 'availability', 'gender', 'active', 'camp_id', 'emergency_profiles'])
         for volunteer in volunteers:
-            writer.writerow(vars(volunteer))
+            writer.writerow([volunteer.user_id, volunteer.username, volunteer.user_password, volunteer.first_name, volunteer.last_name, volunteer.dob, volunteer.user_email, volunteer.contact_number, volunteer.address1, volunteer.address2, volunteer.city, volunteer.acc_type, volunteer.availability, volunteer.gender, volunteer.active, volunteer.camp_id, volunteer.emergency_profiles])
+        # fieldnames = ['user_id', 'username', 'user_password', 'first_name', 'last_name', 'dob', 'user_email',
+        #       'contact_number', 'address1', 'address2', 'city', 'acc_type', 'availability', 'gender', 'active',
+        #       'camp_id', 'emergency_profiles']
+        # writer = csv.DictWriter(cvsfile, fieldnames=fieldnames)
+        # writer.writeheader()
+        # for volunteer in volunteers:
+        #     writer.writerow(vars(volunteer))
 
 # Create the main window
 root = tk.Tk()
@@ -84,13 +88,8 @@ def deactivate_volunteer():
     selected_index = volunteers_listbox.curselection()
     if selected_index:  # Check if a volunteer is selected
         selected_index = selected_index[0]
-        # Get the user_id of the selected volunteer
-        selected_user_id = volunteers[selected_index].user_id
         # Deactivate the selected volunteer
-        for volunteer in volunteers:
-            if volunteer.user_id == selected_user_id:
-                volunteer.active = False
-                break
+        volunteers[selected_index].active = 'False'
         # Save the updated volunteers list to the CSV file
         save_volunteers_to_csv('volunteers_file.csv', volunteers)
         # Show a success message
@@ -100,10 +99,19 @@ def deactivate_volunteer():
         messagebox.showerror("Error", "No volunteer selected.")
 
 def delete_volunteer():
-    # Delete the selected volunteer
-    selected_index = volunteers_listbox.curselection()[0]
-    del volunteers[selected_index]
-    messagebox.showinfo("Success", "Volunteer deleted successfully.")
+    # Get the selected volunteer
+    selected_index = volunteers_listbox.curselection()
+    if selected_index:  # Check if a volunteer is selected
+        selected_index = selected_index[0]
+        # Delete the selected volunteer
+        del volunteers[selected_index]
+        # Save the updated volunteers list to the CSV file
+        save_volunteers_to_csv('volunteers_file.csv', volunteers)
+        # Show a success message
+        messagebox.showinfo("Success", "Volunteer deleted successfully.")
+    else:
+        # Show an error message if no volunteer is selected
+        messagebox.showerror("Error", "No volunteer selected.")
 
 # Create buttons
 load_button = tk.Button(root, text="Load Volunteers", command=load_volunteers)
