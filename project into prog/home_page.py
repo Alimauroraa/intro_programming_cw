22d6393dc645c12ef16b6fc4291d2ee298ac44aa
Gaoren Hao <gaorenh@gmail.com>
2023-11-27 06:36:32 +0000

    need to debug

--- project into prog/home_page.py
+++ project into prog/home_page.py
@@ -78,6 +78,6 @@ def home_page():
 
 if __name__=='__main__':
     home_page()
-    # Create an instance of HumanitarianPlan and call the method
-    humanitarian_plan_instance = HumanitarianPlan(None, None, None, None, None, None, None)
-    humanitarian_plan_instance.generate_missing_camps_from_plans()
+    # No need to create an instance
+    HumanitarianPlan.generate_missing_camps_from_plans()
+
