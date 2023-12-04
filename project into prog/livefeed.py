228b8a51dcf09cdb463f7321b5f0d53fdbba9462
stellachampeaux <zchasc6@ucl.ac.uk>
2023-12-04 18:25:59 +0000

    fix of checkboxes

--- project into prog/livefeed.py
+++ project into prog/livefeed.py
@@ -27,7 +27,7 @@ root.title("Camp message live feed")
 root.configure(bg='#021631')
 
 # Create a frame for the category checkboxes and live feed
-frame = tk.Frame(root, bg = '#021631')
+frame = tk.Frame(root, bg='#021631')
 frame.pack(padx=10, pady=10)
 
 # Filter options for message categories
@@ -36,7 +36,7 @@ category_vars = [tk.IntVar() for _ in category_labels]
 checkboxes = []
 
 for i, category_label in enumerate(category_labels):
-    checkbox = tk.Checkbutton(frame, text=category_label, variable=category_vars[i], bg= "#021631" , fg="white")
+    checkbox = tk.Checkbutton(frame, text=category_label, variable=category_vars[i], bg="#021631", fg="white")
     checkbox.grid(row=i, column=0, sticky="w")
     checkboxes.append(checkbox)
 
@@ -48,12 +48,11 @@ filter_button.grid(row=len(category_labels), column=0, columnspan=2, pady=10)
 live_feed_text = tk.Text(root, wrap=tk.WORD, height=20, width=60)
 live_feed_text.pack(padx=10, pady=10)
 
-# Set the initial state of all checkboxes to selected
+# Set the initial state of all checkboxes to unselected
 for category_var in category_vars:
-    category_var.set(1)
+    category_var.set(0)
 
 # Initially filter and display all updates
 filter_and_display_updates()
 
 root.mainloop()
-
