50a8154e3542e0379e54ad501785b0485fbd835e
Alimauroraa <147698165+Alimauroraa@users.noreply.github.com>
2023-12-13 00:17:42 +0000

    Update home_page.py

--- project into prog/home_page.py
+++ project into prog/home_page.py
@@ -1,5 +1,4 @@
 import tkinter as tk
-#import the login page for volunteer and admin
 from admin_login_gui import Admin
 from create_plan import HumanitarianPlan
 
@@ -7,15 +6,12 @@ def admin_login_page(root):
 
     root.withdraw()
 
-    # Create a Toplevel window for the admin login
     screen_width = root.winfo_screenwidth()
     screen_height = root.winfo_screenheight()
 
-    # Calculate the x and y coordinates to center the window
     x = (screen_width - 400) // 2
     y = (screen_height - 350) // 2
 
-    # Create a Toplevel window for the admin login
     admin_login_window = tk.Toplevel(root)
     admin_login_window.title("Admin Login")
     admin_login_window.geometry(f'400x350+{x}+{y}')
@@ -28,31 +24,19 @@ def open_volunteer_login():
     import GUI_volunteer_login_update
     from GUI_volunteer_login_update import login, show_login_window
     GUI_volunteer_login_update.show_login_window()
-# def open_volunteer_login():
-#     from GUI_volunteer_login_update import login
-#     login()
+
 def home_page():
     bg_color = '#021631'
 
     # main frame initialization
     root = tk.Tk()
     root.title("The Hope Trust: A Humanitarian Management System")
-
-    # Calculate the screen width and height
     root.geometry("400x350+80+90")
-
-    # Set the geometry of the window to center it on the screen
-
 
     root['bg'] = '#021631'
 
     # creating frame
     home = tk.Frame(root, width=600, height=600, bg=bg_color)
-    # admin = tk.Frame(root, bg=bg_color)
-    # volunteer = tk.Frame(root, bg=bg_color)
-    # for frame in (home, admin, volunteer):
-    #home.grid(row=0, column=0, sticky="nsew")
-
     home.pack_propagate(False)
     home.pack()
 
@@ -102,7 +86,6 @@ def home_page():
         activebackground="#B8B8B8",
         activeforeground="black",
         command=lambda: open_volunteer_login()).pack()     #this will open page volunteer login when clicked
-    # root.lift()
     root.mainloop()
 
 if __name__=='__main__':
